c5d8005eee47d7641e8db7b72c73bf3fa9647171
Kavya .S.R <80980373+kavya-sivaguru@users.noreply.github.com>
2021-07-31 18:31:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,26 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader")
 
 let apiQuotes = [];
 
+//show loading
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 function newQuote() {
+    showLoadingSpinner();
+
+    //pick a random quote from apiQuotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
 
     //Check if author field is blank and replace it with quote unknown
@@ -32,13 +48,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    //set quote, hide loader
     quoteText.innerText = quote.text;
-
+    removeLoadingSpinner();
 }
 
-
+//Get quotes from API
 async function getQuotes() {
+    showLoadingSpinner();
     const apiUrl = 'https://type.fit/api/quotes';
 
     try {
